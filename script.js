0479441f3e19bb105fd2ea86e2857180c9d2b35c
Jimmy Lee <39945912+jimmylee719@users.noreply.github.com>
2025-05-07 13:35:51 +0800

    Update script.js

--- script.js
+++ script.js
@@ -106,10 +106,4 @@ window.addEventListener("scroll", () => {
   if (window.scrollY > 300) {
     backToTop.style.display = "block";
   } else {
-    backToTop.style.display = "none";
-  }
-});
-
-backToTop.addEventListener("click", () => {
-  window.scrollTo({ top: 0, behavior: "smooth" });
-});
+    backToTop
